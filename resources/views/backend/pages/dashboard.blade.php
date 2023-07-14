b3fc7549a9baab7d946f1f4a74fe776085f46fcd
JM Udoy <junayedibne.udoy@gmail.com>
2023-07-14 20:15:13 +0600

    Backend and Frontend template setup

---
+++ resources/views/backend/pages/dashboard.blade.php
@@ -0,0 +1,11 @@
+@extends('backend.layouts.master')
+
+@section('title')
+Dashboard
+@endsection
+
+@section('admin_content')
+    <div class="row">
+        <h1>Start From Here</h1>
+    </div>
+@endsection
