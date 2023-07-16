cb563d528c113bcdd91a1752c784b5b1a9197dfc
JM Udoy <junayedibne.udoy@gmail.com>
2023-07-17 00:09:42 +0600

    Category Image added on category table and shown in frontend home page

--- app/Http/Requests/CategoryUpdateRequest.php
+++ app/Http/Requests/CategoryUpdateRequest.php
@@ -23,7 +23,8 @@ class CategoryUpdateRequest extends FormRequest
     {
         return [
             //
-            'title' => 'required|string|max:255'
+            'title' => 'required|string|max:255',
+            'category_image' => 'nullable|image'
         ];
     }
 }
