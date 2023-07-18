52df136727ee95ce9e69327b9125f4da4b756b55
JM Udoy <junayedibne.udoy@gmail.com>
2023-07-18 16:42:53 +0600

    Custom Middleware for admin and customer

--- routes/web.php
+++ routes/web.php
@@ -40,7 +40,7 @@ Route::prefix('')->group(function(){
     Route::get('/login', [RegisterController::class, 'loginPage'])->name('login.page');
     Route::post('/login', [RegisterController::class, 'loginStore'])->name('login.store');
 
-    Route::prefix('customer/')->middleware('auth')->group(function(){
+    Route::prefix('customer/')->middleware(['auth', 'is_customer'])->group(function(){
         Route::get('dashboard',[CustomerController::class, 'dashboard'])->name('customer.dashboard');
         Route::get('logout', [RegisterController::class, 'logout'])->name('customer.logout');
     });
@@ -53,8 +53,9 @@ Route::prefix('admin/')->group(function(){
     Route::post('login', [LoginController::class, 'login'])->name('admin.login');
     Route::get('logout', [LoginController::class, 'logout'])->name('admin.logout');
 
-    Route::middleware(['auth'])->group(function(){
+    Route::middleware(['auth', 'is_admin'])->group(function(){
         Route::get('dashboard', [DashboardController::class, 'dashboard'])->name('admin.dashboard');
+        Route::get('logout', [LoginController::class, 'logout'])->name('admin.logout');
     });
 
     /*Resource Controller*/
