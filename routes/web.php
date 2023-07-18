93d4cbca0e8ee7d9abb261a5f9319ab632271c33
JM Udoy <junayedibne.udoy@gmail.com>
2023-07-18 14:07:17 +0600

    Customer register and login process & redirect to Dashboard

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,9 @@ use App\Http\Controllers\Backend\CouponController;
 use App\Http\Controllers\Backend\ProductController;
 use App\Http\Controllers\Backend\CategoryController;
 use App\Http\Controllers\Backend\DashboardController;
+use App\Http\Controllers\Frontend\CustomerController;
 use App\Http\Controllers\Backend\TestimonialController;
+use App\Http\Controllers\Frontend\Auth\RegisterController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -31,15 +33,30 @@ Route::prefix('')->group(function(){
     Route::get('/shopping-cart', [CartController::class, 'cartPage'])->name('cart.page');
     Route::post('/add-to-cart', [CartController::class, 'addToCart'])->name('add-to.cart');
     Route::get('/remove-from-cart/{cart_id}', [CartController::class, 'removeFromCart'])->name('removefrom.cart');
+
+    /*Authentication routes for Customer/Guest */
+    Route::get('/register', [RegisterController::class, 'registerPage'])->name('register.page');
+    Route::post('/register', [RegisterController::class, 'registerStore'])->name('register.store');
+    Route::get('/login', [RegisterController::class, 'loginPage'])->name('login.page');
+    Route::post('/login', [RegisterController::class, 'loginStore'])->name('login.store');
+
+    Route::prefix('customer/')->middleware('auth')->group(function(){
+        Route::get('dashboard',[CustomerController::class, 'dashboard'])->name('customer.dashboard');
+        Route::get('logout', [RegisterController::class, 'logout'])->name('customer.logout');
+    });
 });
+
+
 /*Admin Auth routes */
 Route::prefix('admin/')->group(function(){
     Route::get('login', [LoginController::class, 'loginPage'])->name('admin.loginpage');
     Route::post('login', [LoginController::class, 'login'])->name('admin.login');
     Route::get('logout', [LoginController::class, 'logout'])->name('admin.logout');
+
     Route::middleware(['auth'])->group(function(){
         Route::get('dashboard', [DashboardController::class, 'dashboard'])->name('admin.dashboard');
     });
+
     /*Resource Controller*/
     Route::resource('category', CategoryController::class);
     Route::resource('testimonial', TestimonialController::class);
