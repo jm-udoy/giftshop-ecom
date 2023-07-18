34be3de4a223e3d112b11e8b3d832bd1f912f97f
JM Udoy <junayedibne.udoy@gmail.com>
2023-07-18 21:37:15 +0600

    Checkout Page and ajax based dynamic district & upazila load in form

--- resources/views/frontend/layouts/inc/style.blade.php
+++ resources/views/frontend/layouts/inc/style.blade.php
@@ -23,3 +23,5 @@
 <script src="{{ asset('assets/frontend') }}/js/vendor/modernizr-2.8.3.min.js"></script>
 
 <link rel="stylesheet" href="http://cdn.bootcss.com/toastr.js/latest/css/toastr.min.css">
+
+@stack('frontend_style')
