c61d9070059fe8d12d923272d6f113dbde3479a8
JM Udoy <junayedibne.udoy@gmail.com>
2023-08-02 23:03:41 +0600

    Design Changes

--- resources/views/frontend/pages/widgets/slider.blade.php
+++ resources/views/frontend/pages/widgets/slider.blade.php
@@ -9,7 +9,7 @@
                                 <div class="col-lg-12 col-lg-9 col-12">
                                     <div class="slider-content">
                                         <div class="slider-shape">
-                                            <h2 data-swiper-parallax="-500">Amazing Pure Nature Hohey</h2>
+                                            <h2 data-swiper-parallax="-500">Amazing Gifts</h2>
                                             <p data-swiper-parallax="-400">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin</p>
                                             <a href="{{ route('shop.page') }}" data-swiper-parallax="-300">Shop Now</a>
                                         </div>
@@ -26,7 +26,7 @@
                                 <div class="col-lg-12 col-lg-9 col-12">
                                     <div class="slider-content">
                                         <div class="slider-shape">
-                                            <h2 data-swiper-parallax="-500">Amazing Pure Nature Coconut Oil</h2>
+                                            <h2 data-swiper-parallax="-500">Amazing Gifts</h2>
                                             <p data-swiper-parallax="-400">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin</p>
                                             <a href="{{ route('shop.page') }}" data-swiper-parallax="-300">Shop Now</a>
                                         </div>
@@ -43,7 +43,7 @@
                                 <div class="col-lg-12 col-lg-9 col-12">
                                     <div class="slider-content">
                                         <div class="slider-shape">
-                                            <h2 data-swiper-parallax="-500">Amazing Pure Nut Oil</h2>
+                                            <h2 data-swiper-parallax="-500">Amazing Gifts</h2>
                                             <p data-swiper-parallax="-400">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin</p>
                                             <a href="{{ route('shop.page') }}" data-swiper-parallax="-300">Shop Now</a>
                                         </div>
